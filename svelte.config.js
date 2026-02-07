dfc905d20bdb90b06c4754217feb13b21e260f30
watchakorn-18k <wk18k@proton.me>
2026-02-08 04:44:50 +0700

    update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,9 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
+		paths: {
+			base: '/khun-phaen-tracker-offline'
+		},
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
